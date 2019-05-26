bef09214163cbb5bac749bfa48b0d4532afa7993
cameliaM <camelia.macariu@yahoo.com>
2019-05-27 00:44:59 +0300

    Deals page

--- Web/my-app/src/components/DealComponent/DealComponent.jsx
+++ Web/my-app/src/components/DealComponent/DealComponent.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import photoPath from '../../assets/douglas.png'
 import './DealComponent.css';
 
 class DealComponent extends Component {
-    title = 'Douglas';
-    description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard. '
-    discount = 5;
-    category = 'Beauty';
-
     render() {
         return (
             <div class="deal-container">
-                <div class="category">{this.category}</div>
+                <div class="category">{this.props.category}</div>
                 <div class="photo">
-                    <img src={photoPath}></img>
+                    <img src={"images/"+this.props.photoName+".png"} alt=""></img>
                 </div>
                 <div class="info">
-                    <div class="info-title">{this.title}</div>
-                    <div class="info-description">{this.description}</div>
-                    <div class="info-discount">{this.discount}% DISCOUNT</div>
+                    <div class="info-title">{this.props.title}</div>
+                    <div class="info-description">{this.props.description}</div>
+                    <div class="info-discount">{this.props.discount}
+                        {this.props.discount != null ? "% DISCOUNT" : this.props.promotion}
+                    </div> 
                 </div>
             </div>
         );
--- Web/my-app/src/components/DealsPage/DealsPage.jsx
+++ Web/my-app/src/components/DealsPage/DealsPage.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
 import DealComponent from '../DealComponent/DealComponent';
+import notificationPath from '../../assets/notification.png'
 import './DealsPage.css';
 
 class DealsPage extends Component {
     render() {
         return (
-            <div>
-                <h2>Welcome to Deals Page!</h2>
-                <div>
-                    <DealComponent/>
+            <div class="deals-container">
+                <div class="notification">
+                    <img src={notificationPath} alt=""></img>
+                </div>
+                <div class="search-bar">
+                    <div class="search">
+                        <form class="search-form">
+                            <input type="text" placeholder="Type a deal.."/>
+                            <input type="submit" value="Search"/>
+                        </form>
+                    </div>
+                </div>
+                <div class="deals-title">All Deals</div>
+                <div class="deals-subtitle">Today best deals</div>
+                <div class="deal-card">
+                    <DealComponent 
+                        title={"Douglas"}
+                        description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard."}
+                        discount={5}
+                        category={"Beauty"}
+                        photoName={"douglas"}/>
+                </div>
+                <div class="deal-card">
+                    <DealComponent 
+                        title={"H&M"}
+                        description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard."}
+                        discount={20}
+                        category={"Fashion"}
+                        photoName={"hm"}/>
+                </div>
+                <div class="deal-card">
+                    <DealComponent 
+                        title={"McDonald's"}
+                        description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard."}
+                        promotion={"2+1 FREE"}
+                        category={"Food"}
+                        photoName={"mc"}/>
                 </div>
             </div>
         );
