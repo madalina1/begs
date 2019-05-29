71ba366ac136b3f2e62f43e889340631eb10dad4
Madalina Insuratelu <Madalina.Insuratelu@endava.com>
2019-05-29 11:44:26 +0300

    Added possibility to upload qr on QR page

--- Web/my-app/src/components/QRPage/QRPage.jsx
+++ Web/my-app/src/components/QRPage/QRPage.jsx
@@ -1,34 +1,65 @@
 import React, { Component } from 'react';
 import QrReader from 'react-qr-reader'
+
 import './QRPage.css';
 
 class QRPage extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
           delay: 300,
           message: "Please scan the QR code",
+          result: 'No result',
+          granted: false
         };
+
         this.handleScan = this.handleScan.bind(this);
+        this.openImageDialog = this.openImageDialog.bind(this);
+        this.handlePermissions = this.handlePermissions.bind(this);
       }
+
       handleScan(data) {
         if (data) {
           this.setState({ result: data });
         }
       }
+
       handleError(err) {
         console.error(err);
       }
+
+      openImageDialog() {
+        this.refs.qrReader1.openImageDialog()
+      }
+
+      handlePermissions() {
+        this.setState({
+          granted: !this.state.granted
+        });
+      }
+
       render() {
+        const { delay, message, granted } = this.state;
+
         return (
-          <div>
+          <div className="qr-container">
             <QrReader 
-              delay={this.state.delay}
+              ref="qrReader1"
+              delay={delay}
               onError={this.handleError}
               onScan={this.handleScan}
               style={{ width: "100%", height: "100%"}}
+              legacyMode={granted}
+              className="qr-section"
             />
-            <p className="qr-description">{this.state.message}</p>
+            
+            <button className="qr-upload" onClick={this.handlePermissions}>{granted ? 'Go to the Camera' : 'Upload the QR code'}</button>
+            {
+              granted && 
+              <button className="qr-upload qr-submit" onClick={this.openImageDialog}>Upload image</button>
+            }
+            <p className="qr-description">{message}</p>
           </div>
         );
       }
